f9d61afbcdfeab81ea0f88f373183d699af54ce7
DESKTOP-88IF2P3\George Stratulat <george.stratulat10@gmail.com>
2017-03-03 13:53:45 +0100

    //salut

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+/**
+ * Created by George Stratulat on 03/03/2017.
+ */
+public class Person {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
